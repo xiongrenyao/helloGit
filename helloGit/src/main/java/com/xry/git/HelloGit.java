63f426fdb3acdbabf14fe8356aaf968e419bada5
xiongrenyao <1354856763@qq.com>
2020-08-06 12:15:26 +0800

    创建分支

--- helloGit/src/main/java/com/xry/git/HelloGit.java
+++ helloGit/src/main/java/com/xry/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.err.println("hello Git!");
         System.err.println("更新1");
+        System.err.println("创建分支");
     }
 }
